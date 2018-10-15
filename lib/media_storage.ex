a7c1a863b2e14d99785ca65b414c9c9a42b71e24
Nick Kutryj <nick@tokagen.com>
2018-10-15 11:20:59 +0200

    Initial commit.

---
+++ lib/media_storage.ex
@@ -0,0 +1,18 @@
+defmodule MediaStorage do
+  @moduledoc """
+  Documentation for MediaStorage.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MediaStorage.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/media_storage_test.exs
@@ -0,0 +1,8 @@
+defmodule MediaStorageTest do
+  use ExUnit.Case
+  doctest MediaStorage
+
+  test "greets the world" do
+    assert MediaStorage.hello() == :world
+  end
+end
